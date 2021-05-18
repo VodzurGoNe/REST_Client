129b18040894c40d8b22d5f1156f2d33b63a9d13
Vladislav Gruzdov <78535650+VodzurGoNe@users.noreply.github.com>
2021-05-18 15:44:58 +0400

    Add files via upload

---
+++ spring_course_rest_client/src/main/java/org/gruzdov/spring/rest/App.java
@@ -0,0 +1,13 @@
+package org.gruzdov.spring.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
